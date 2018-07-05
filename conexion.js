865ef087852da4e120bff30382e722fe379cdc34
Ignacio Olvera <ignacio.olvera766@yahoo.com.mx>
2018-07-05 10:16:26 -0500

    Cambios de Local

--- conexion.js
+++ conexion.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "us-cdbr-iron-east-04.cleardb.net",
-    user: "b6cb2661d1e790",
-    password: "69318445bc2b71f",
-    database:"heroku_2585833ccd9edf4",
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database:"qmc_mrp",
     timezone: 'utc'
 });
 module.exports=con;
