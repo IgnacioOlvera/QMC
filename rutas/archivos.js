c0dcea29c42f3eda7faaab29f85e653adb3f33d0
Ignacio Olvera <ignacio.olvera766@yahoo.com.mx>
2018-07-06 10:51:37 -0500

    Configuración de Consultas

--- rutas/archivos.js
+++ rutas/archivos.js
@@ -758,7 +758,7 @@ api.post('/BillOfLanding', function (req, res) {
             }
         }
     });
-    wb.write(`../docs/Bill Of Landing ${info.cliente[0].nombre} ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}.xlsx`, function (err) {
+    wb.write(path.join(__dirname,`../docs/Bill Of Landing ${info.cliente[0].nombre} ${fecha.getDate()} de ${meses[fecha.getMonth()]} de ${fecha.getFullYear()}.xlsx`), function (err) {
         if (err) throw err
         else {
             res.send({ message: 'Archivo creado', status: '200' });
