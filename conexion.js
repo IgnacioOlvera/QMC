6ed23475057c2a365446b6e3abcad937e1b630cf
Ignacio Olvera <ignacio.olvera766@yahoo.com.mx>
2018-07-08 19:17:48 -0500

    Config

--- conexion.js
+++ conexion.js
@@ -1,10 +1,9 @@
 var mysql = require('mysql');
 var con = mysql.createConnection({
-    host: "g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "exvmv4cox4vu19dq",
-    password: "nbxqrzgjrst4fvh5",
-    database:"sgqd0fcpu2lki8lm",
-
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database:"qmc_mrp",
     timezone: 'utc'
 });
 module.exports=con;
